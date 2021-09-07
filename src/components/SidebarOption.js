e934726058b82739534db1fd0f53d3839c6bb4c8
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-07 17:52:28 +0200

    building Sidebar Options

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/SidebarOption.css';
 
-function SidebarOption() {
+function SidebarOption({ Icon, title, number, selected }) { // props distructured
     return (
-        <div className="sidebarOption">
-            
+        // <div className="sidebarOption"> // for default selected, we transform the class to ...
+        <div className={`sidebarOption ${selected && "sidebarOption--active"}`}> 
+            <Icon />
+            <h3>{title}</h3>
+            <p>{number}</p>
         </div>
     );
 }
