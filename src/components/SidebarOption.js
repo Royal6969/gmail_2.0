c85be586db260a2b2f5f5317b6bc8d8b0da659f2
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-07 15:08:45 +0200

    building Sidebar component - header ComposeButtom

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/SidebarOption.css';
+
+function SidebarOption() {
+    return (
+        <div className="sidebarOption">
+            
+        </div>
+    );
+}
+
+export default SidebarOption;
