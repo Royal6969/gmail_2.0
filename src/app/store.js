297ce9e5fe781eb5725f2a4e68688db132b2bf51
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-09 12:36:20 +0200

    building mail screen - creating mailSlice for setting up mailReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counterSlice';
+//import counterReducer from '../features/counterSlice'; // I have replaced this for the below that I have created for this...
+import mailReducer from '../features/mailSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    mail: mailReducer,
   },
 });
