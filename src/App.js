f32e6cab28a5a9ca4efb653a1d88a8e389470f14
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-14 12:58:04 +0200

    create user actions - signIn and SignOut - signIn with Google

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
@@ -6,8 +6,12 @@ import { BrowserRouter as Router, Switch, Route, /*Link,*/ } from 'react-router-
 import Mail from './components/Mail';
 import EmailList from './components/EmailList';
 import SendMail from './components/SendMail';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { login } from './features/userSlice';
+import { selectUser } from './features/userSlice';
+import Login from './components/Login';
+import { auth } from './firebase';
 
 // proyect started with:
 // npx create-react-app gmail-clone --template redux
@@ -15,28 +19,49 @@ function App() {
 
   // this is how we get a piece os state
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if(user) {
+        // the user is logged in ... (it is like a "login persistance")
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL,
+        }))
+      }
+    })
+  }, [dispatch]);
 
   return (
     // npm install react-router-dom
     <Router>
-      <div className="app">
-        <Header />
 
-        <div className="app__body">
-          <Sidebar />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
 
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
+          <div className="app__body">
+            <Sidebar />
+
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+
+          {sendMessageIsOpen && <SendMail />}
         </div>
+      )}
 
-        {sendMessageIsOpen && <SendMail />}
-      </div>
     </Router>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 //import counterReducer from '../features/counterSlice'; // I have replaced this for the below that I have created for this...
 import mailReducer from '../features/mailSlice';
+import userReducer from '../features/userSlice';
 
 export const store = configureStore({
   reducer: {
     mail: mailReducer,
+    user: userReducer,
   },
 });
